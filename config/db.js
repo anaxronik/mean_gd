5ff2f35f18912f75b464b61f5d004130c2463b91
Sergey Komarov <anaxronik@gmail.com>
2020-04-27 18:00:55 +0500

    create connection to local DB

---
+++ config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: "mongodb://localhost:27017/website",
+    remoteDB: "mongodb://dbuser:Ae1757484@mongodb-g35bl.mongodb.net/test?retryWrites=true&w=majority",
+    secret: 'meanstackanxsecretkey'
+}
--- index.js
+++ index.js
@@ -4,19 +4,48 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const path = require('path')
+const config = require('./config/db')
 
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
+// необходим чтобы сайт мог использовать постороние API
+app.use(cors())
+
+app.use(bodyParser.json())
+
 // подключение папки с статическими файлами
 app.use(express.static(path.join(__dirname, 'public')))
+
+
+// подключение к базе данных
+mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection.on('connected', () => {
+    console.log('==> Connect to DB')
+})
+mongoose.connection.on('error', (err) => {
+    console.log('==x NOT connect to DB', err)
+})
+
 
 app.get('/', (req, res) => {
     res.send('Main Page')
 })
 
+app.get('/account/reg', (req, res) => {
+    res.send('/account/reg Page')
+})
+
+app.get('/account/auth', (req, res) => {
+    res.send('/account/auth Page')
+})
+
+app.get('/account/dashboard', (req, res) => {
+    res.send('/account/dashboard Page')
+})
+
 app.listen(PORT, () => {
-    console.log('===> Server start on port:', PORT)
+    console.log('=> Server start on port:', PORT)
 })
