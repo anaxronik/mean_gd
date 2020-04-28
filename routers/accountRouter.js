74882dfa944e414f086f64c045e27bcd3f28d972
Sergey K <anaxronik@gmail.com>
2020-04-28 09:37:28 +0500

    add method post in refistrarion route

--- routers/accountRouter.js
+++ routers/accountRouter.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user');
 
 
-router.get('/reg', (req, res) => {
-    res.send('/account/reg Page')
+// router.get('/reg', (req, res) => {
+//     res.send('/account/reg Page')
+// })
+
+router.post('/reg', (req, res) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        login: req.body.login,
+        password: req.body.password,
+    })
+    User.addUser(newUser, (err, user) => {
+        if (err)
+            res.json({ success: false, message: 'User is NOT add' })
+        else
+            res.json({ success: true, message: 'User has been add' })
+
+    })
+    // res.send('/account/reg Page')
 })
 
 router.get('/auth', (req, res) => {
